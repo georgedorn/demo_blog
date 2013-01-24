d40c2f75dbe17d37091ad24be938d88ebb654c09
georgedorn <georgedorn@gmail.com>
2013-01-23 16:12:05 -0800

    missed one

--- demo_blog/blog/urls.py
+++ demo_blog/blog/urls.py
@@ -5,7 +5,7 @@ from views import ViewPost, ListPosts, EditPost
 urlpatterns = patterns('',
     url(r'^$', ListPosts.as_view(), name='post-list'),
     url(r'^(?P<slug>[-_\w]+)/$', ViewPost.as_view(), name='post-detail'),
-    url(r'^(?P<slug>[-_\w]+)/edit/$', EditPost.as_view(), name='post-edit'),
     url(r'^(?P<slug>[-_\w]+)/create/$', EditPost.as_view(), name='post-edit'),
+    url(r'^(?P<slug>[-_\w]+)/edit/$', EditPost.as_view(), name='post-edit'),
     url(r'^(?P<slug>[-_\w]+)/delete/$', EditPost.as_view(), name='post-edit'),
 )
