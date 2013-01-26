ec750cb49cf02c35203dc4b6a06cec5937d9c598
georgedorn <georgedorn@gmail.com>
2013-01-25 19:20:03 -0800

    Added links to edit/delete on post list, added comment model, minor form tweaks

--- demo_blog/blog/forms.py
+++ demo_blog/blog/forms.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Post
 
-class PostForm(ModelForm):
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('owner')
-    
+        widgets = {
+                   'title': forms.TextInput()
+                   
+                   }
--- demo_blog/blog/models.py
+++ demo_blog/blog/models.py
@@ -19,7 +19,33 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse_lazy('post-detail', kwargs={'slug':self.slug})
+    
+    def get_edit_url(self):
+        return reverse_lazy('post-edit', kwargs={'slug':self.slug})
+
+    def get_delete_url(self):
+        return reverse_lazy('post-delete', kwargs={'slug':self.slug})
         
-    
-    
 admin.site.register(Post)
+    
+    
+class Comment(models.Model):
+    """
+    Represents a comment on a post.
+    
+    django.contrib.comment not used as it's too
+    generic and difficult to extend to change behaviors.
+    """
+    
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User, null=True, blank=True) #if a user is logged in, relate this comment to them.
+    user_name = models.TextField()
+    content = models.TextField()
+    
+    parent = models.ForeignKey('Comment', null=True, default=None) #for threaded comments, later
+    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    
+    
